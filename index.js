21b6c8f3d765412518671ea37b1dc44d257860bf
pauladarias <pauladarias@gmail.com>
2020-10-02 10:33:38 +0100

    Add hidenAllSlides function

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ document
   .getElementById("carousel-button-prev")
   .addEventListener("click", moveToPrevSlide);
 
+function hideAllSlides() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
+  }
+}
+
 function moveToNextSlide() {
+  hideAllSlides();
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
@@ -20,5 +28,11 @@ function moveToNextSlide() {
 }
 
 function moveToPrevSlide() {
-  console.log("im working");
+  hideAllSlides();
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition--;
+  }
+  slides[slidePosition].classList.add("carousel-item-visible");
 }
